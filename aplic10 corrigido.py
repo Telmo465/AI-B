cc40619abdd7b5abe5a15529801287c196a57e88
TelmoUjunior <30441457+TelmoUjunior@users.noreply.github.com>
2017-11-17 19:04:23 -0100

    Add files via upload

---
+++ aplic10 corrigido.py
@@ -0,0 +1,27 @@
+import random
+n=int(input("Número de linhas"))
+m=int(input("Número de colunas"))
+v=[]
+t=0
+a=[[0 for j in range(m)] for i in range(n)]
+for i in range(n):
+    for j in range(m):
+        a[i][j]=random.randint(1,10)
+for i in range(len(a)):
+    s=1
+    g=0
+    for j in range(len(a[0])):
+        if(a[i][j] %2) != 0:
+            s*=a[i][j]
+            if a[i][j]== 1:
+                g+=1
+            print(g)
+    if s==1 and g==0:
+        v.append(0)
+    else:
+        v.append(s)
+for x in range(len(v)):
+    t+=v[x]
+print(a)
+print(v)
+print(t)
